0c0e47be6ecccbfe627577c03f55279ba1afea23
jyoti <jyotidhage10@gmail.com>
2022-09-19 14:56:13 +0530

    [Aaradhya]Completed UC-1 by using JS

---
+++ EmployeePresentAbsent.js
@@ -0,0 +1,7 @@
+const Is_Absent =0;
+let empCheck =Math.floor(Math.random() * 10) % 2;
+if(empCheck==Is_Absent){
+    console.log("UC-1 Employee is Absent");
+}else{
+    console.log("UC-1 Employee is Present");
+}
